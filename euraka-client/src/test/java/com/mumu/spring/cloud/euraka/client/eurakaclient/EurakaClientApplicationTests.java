8910b9b832fbb8824dd346ce1c7bf3b6eb5846ed
Wangdamu <wwwwshuguang@163.com>
2017-10-12 17:00:09 +0800

    spring cloud学习

---
+++ euraka-client/src/test/java/com/mumu/spring/cloud/euraka/client/eurakaclient/EurakaClientApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mumu.spring.cloud.euraka.client.eurakaclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EurakaClientApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
